697cdf0c22befe9da209fec8125d283c359779a4
arpanchauhan1608-lang <arpan.chauhan1608@gmail.com>
2025-09-18 10:48:11 +0530

    initail commit

---
+++ task2.py
@@ -0,0 +1,7 @@
+import math
+
+num = float(input("Enter a number: "))
+
+print(f"Square root of {num} = {math.sqrt(num)}")
+print(f"Natural log of {num} = {math.log(num)}")
+print(f"Sine of {num} radians = {math.sin(num)}")
